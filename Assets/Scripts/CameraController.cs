4f7589e1d37a551fd12d06d1aac840eb63c3998e
Innominate-Dev <InnominateDev@gmail.com>
2023-01-09 15:39:24 +0000

    Added Player Camera
    - Added Camera Script
    
    - Doesn't work just yet

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float MouseSensetivity = 100f;
+
+    public Transform playerObj;
+
+    float xRotation = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //float mouseX = Input.GetAxis(mouseX) * MouseSensetivity * Time.deltaTime;
+        //float mouseY = Input.GetAxis(mouseY) * MouseSensetivity * Time.deltaTime;
+
+        //xRotation -= mouseY;
+        //xRotation = Mathf.Clamp(xRotation, 90f, 90f);
+
+    }
+}
+

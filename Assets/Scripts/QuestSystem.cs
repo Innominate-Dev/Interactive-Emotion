54dfba2080e5e725394f323936a931487e78753c
Innominate-Dev <InnominateDev@gmail.com>
2023-01-13 13:11:25 +0000

    Added Dialogue System
    - Added Dialogue
    - Added Interaction
    - Added UI
    
    - Added Physics Material
    - Added TMPro

--- Assets/Scripts/QuestSystem.cs
+++ Assets/Scripts/QuestSystem.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class QuestSystem : MonoBehaviour
 {
@@ -10,6 +12,9 @@ public class QuestSystem : MonoBehaviour
 
     public float DistanceX = 1f;
     public float DistanceY = 1f;
+
+    public RawImage UICrosshair;
+    public TextMeshProUGUI Interaction;
 
     // Start is called before the first frame update
     void Start()
@@ -22,12 +27,33 @@ public class QuestSystem : MonoBehaviour
     {
         Ray ray = cam.ViewportPointToRay(new Vector3(DistanceX, DistanceY, 0));
         RaycastHit hit;
-        
-        if (Physics.Raycast(ray, out hit))
-            print("I'm looking at" + hit.transform.name);
-        else
-            print("Im looking at nothing");
 
-        Debug.DrawLine(transform.position, hit.point, Color.green);
+        if (Physics.Raycast(ray, out hit))
+            if(hit.collider.CompareTag("NPC"))
+            {
+                InteractionActive();
+            }
+            else
+            {
+                InteractionDeactive();
+            }
+        else
+        {
+            InteractionDeactive();
+        }
+
+        Debug.DrawLine(transform.position, hit.point, Color.blue);
+    }
+
+    public void InteractionActive()
+    {
+        UICrosshair.gameObject.SetActive(false);
+        Interaction.gameObject.SetActive(true);
+    }
+
+    public void InteractionDeactive()
+    {
+        UICrosshair.gameObject.SetActive(true);
+        Interaction.gameObject.SetActive(false);
     }
 }

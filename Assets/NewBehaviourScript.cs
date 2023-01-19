e9efb66cb50bbbb917a435a432f0ef1de3184427
Innominate-Dev <InnominateDev@gmail.com>
2023-01-19 12:44:44 +0000

    Updated Switching Textures
    - Upon Colliding with the wall it switches colour
    - Added Coroutine to help switch textures.

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    public Texture NewTexture;
+    public Texture[] NewTexture;
+    public int CurrentTexture;
+
+    public bool ChangedTexture = false;
+
+    public Renderer m_renderer;
+
+    private void Awake()
+    {
+        
+    }
+
+
 
     // Start is called before the first frame update
     void Start()
     {
         Material myMat = GetComponent<Renderer>().material;
-        Renderer m_renderer = GetComponent<Renderer>();
-        myMat.SetTexture("MainTex",NewTexture);
+        m_renderer = GetComponent<Renderer>();
+        //myMat.SetTexture(1,NewTexture);
         //Debug.Log(myMat.mainTexture.ToString());
+
+        ///////////////////////////////////////
+
     
     }
 
@@ -20,5 +35,21 @@ public class NewBehaviourScript : MonoBehaviour
     void Update()
     {
         
+    }
+    private void OnParticleCollision(GameObject other)
+    {
+        if(other.tag == "PaintCollision" && ChangedTexture == false)
+        {
+            StartCoroutine(TextureSwap());
+            ChangedTexture = true;
+        }
+    }
+
+    IEnumerator TextureSwap()
+    {
+        yield return new WaitForSeconds(1f);
+        CurrentTexture++;
+        CurrentTexture %= NewTexture.Length;
+        m_renderer.material.mainTexture = NewTexture[CurrentTexture];
     }
 }

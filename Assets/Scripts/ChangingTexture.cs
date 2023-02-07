abf248ff338f1ef6d0df43d9243cfcd3d71de8c9
Innominate-Dev <InnominateDev@gmail.com>
2023-02-07 12:47:13 +0000

    Updated City Painted Progress
    - Added Percentage
    - Added filler bar that changes and is reacted to the player painting.

--- Assets/Scripts/ChangingTexture.cs
+++ Assets/Scripts/ChangingTexture.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class ChangingTexture : MonoBehaviour
 {
@@ -12,12 +13,25 @@ public class ChangingTexture : MonoBehaviour
 
     public Renderer m_renderer;
 
+    RectTransform fill;
+
     public static int paintedObjects = 0;
-    public Slider Progress;
+    public static int totalPaintedObjects;
+    float startingScale;
+
+    //ublic Slider Progress;
+    float progress;
+    int PercentageProgress;
+
+    public RectTransform handle;
+    public RectTransform splatPoint;
+    public RectTransform PercentagePOS;
+    public TextMeshProUGUI Percentage;
 
     private void Awake()
     {
         var myList = FindObjectsOfType<ChangingTexture>();
+        totalPaintedObjects = myList.Length;
     }
 
 
@@ -25,16 +39,28 @@ public class ChangingTexture : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        fill = GameObject.Find("Fill").GetComponent<RectTransform>();
+        handle = GameObject.Find("Handle").GetComponent<RectTransform>();
+        splatPoint = GameObject.Find("Splatpoint").GetComponent<RectTransform>();
+        Percentage = GameObject.Find("CityPaintedPercentage").GetComponent<TextMeshProUGUI>();
+        PercentagePOS = GameObject.Find("CityPaintedPercentage").GetComponent<RectTransform>();
+
         Material myMat = GetComponent<Renderer>().material;
         m_renderer = GetComponent<Renderer>();
         //myMat.SetTexture(1,NewTexture);
         //Debug.Log(myMat.mainTexture.ToString());
 
+        startingScale = fill.transform.lossyScale.x;
+
+        fill.transform.localScale = new Vector3(progress, fill.transform.localScale.y, fill.transform.localScale.z);
+        handle.position = splatPoint.position;
     }
 
     // Update is called once per frame
     void Update()
     {
+        progress = (float)paintedObjects / (float)totalPaintedObjects;
+
         //Debug.Log(paintedObjects);
     }
     private void OnParticleCollision(GameObject other)
@@ -43,7 +69,14 @@ public class ChangingTexture : MonoBehaviour
         {
             StartCoroutine(TextureSwap());
             paintedObjects++;
-            Progress.value = paintedObjects;
+
+            fill.transform.localScale = new Vector3(progress, fill.transform.localScale.y, fill.transform.localScale.z);
+            handle.position = splatPoint.position;
+            PercentagePOS.position = splatPoint.position;
+
+            PercentageProgress = ((int)progress);
+            Percentage.text = (PercentageProgress + "%");
+
             ChangedTexture = true;
         }
     }
